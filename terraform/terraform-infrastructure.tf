f45adecfe36f63f5a7144c3ea0142126e87b9df2
sxarp <you@example.com>
2019-07-03 12:33:58 +0900

    Add lock table for aws provider.

--- terraform/terraform-infrastructure.tf
+++ terraform/terraform-infrastructure.tf
@@ -2,3 +2,17 @@ resource "aws_s3_bucket" "terraform-backend" {
   bucket = "terraform-backend-for-eks-sxarp"
   acl    = "private"
 }
+
+resource "aws_dynamodb_table" "eks-sample-lock-table" {
+  name           = "EKSSampleTfLock"
+  billing_mode   = "PAY_PER_REQUEST"
+
+  # `LockId`ではなく`LockID`なのに注意
+  # 参考: https://github.com/hashicorp/terraform/issues/12877#issuecomment-289920798
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
