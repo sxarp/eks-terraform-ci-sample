e01f4f204eee89c6ef6f9725df6f3401e45457d2
sxarp <you@example.com>
2019-07-04 12:10:39 +0900

    Add SG for worker nodes.

--- terraform/worker_node.tf
+++ terraform/worker_node.tf
@@ -37,3 +37,46 @@ resource "aws_iam_instance_profile" "sample-node" {
   name = "terraform-eks-sample"
   role = "${aws_iam_role.sample-node.name}"
 }
+
+# SG関連
+# 参考: https://docs.aws.amazon.com/eks/latest/userguide/sec-group-reqs.html
+
+resource "aws_security_group" "sample-node" {
+  name        = "terraform-eks-sample-node"
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = "${aws_vpc.sample.id}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = "${
+    map(
+     "Name", "terraform-eks-sample-node",
+     "kubernetes.io/cluster/${var.cluster-name}", "owned",
+    )
+  }"
+}
+
+resource "aws_security_group_rule" "sample-node-ingress-self" {
+  description              = "Allow node to communicate with each other"
+  from_port                = 0
+  protocol                 = "-1"
+  security_group_id        = "${aws_security_group.sample-node.id}"
+  source_security_group_id = "${aws_security_group.sample-node.id}"
+  to_port                  = 65535
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "sample-node-ingress-cluster" {
+  description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
+  from_port                = 1025
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.sample-node.id}"
+  source_security_group_id = "${aws_security_group.sample-cluster.id}"
+  to_port                  = 65535
+  type                     = "ingress"
+}
