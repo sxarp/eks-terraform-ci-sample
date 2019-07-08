15ad61a01429fd778d4a2f447b27dce5650c36a5
sxarp <you@example.com>
2019-07-08 13:32:40 +0900

    Add ecr for app image.

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -2,3 +2,8 @@
 resource "aws_ecr_repository" "ci_for_k8s" {
   name = "${var.cluster-name}/ci_for_k8s"
 }
+
+# app本体のimageの置き場所
+resource "aws_ecr_repository" "app" {
+  name = "${var.cluster-name}/app"
+}
