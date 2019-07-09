9210e28a0e86e1949c3901e4ae4f54d293285a47
sxarp <you@example.com>
2019-07-09 21:46:54 +0900

    Add ALB listener.

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -21,6 +21,17 @@ resource "aws_lb" "sample" {
   }
 }
 
+resource "aws_lb_listener" "sample" {
+  load_balancer_arn = "${aws_lb.sample.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_alb_target_group.sample.arn}"
+  }
+}
+
 resource "aws_security_group" "sample-alb" {
   name        = "terraform-eks-sample-alb"
   description = "Security group for alb"
